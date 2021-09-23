90a41203c55f1a122c88da7cf21865cbee7c0ee5
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2021-09-23 19:12:20 +0100

    Update default environment file

--- environment.js
+++ environment.js
@@ -1,7 +1,15 @@
 window.env = {
-	// URL to the Reconmap API including protocol and port but not trailing slash
-    REACT_APP_API_ENDPOINT: 'http://localhost:8080',
+    REACT_APP_SECURE_TRANSPORT_ENABLED: false,
 
-	// URL to the Reconmap WebSocket API including protocol and port but not trailing slash
-    REACT_APP_WS_ENDPOINT: 'ws://localhost:8765'
+    // Reconmap API's URL including protocol and port but not trailing slash
+    REACT_APP_DEFAULT_API_URL: 'http://localhost:8080',
+
+    // hostname:port for the Reconmap's notifications service
+    REACT_APP_NOTIFICATIONS_SERVICE_HOST_PORT: 'localhost:4040',
+
+    // hostname:port for the Reconmap's agent service
+    REACT_APP_AGENT_SERVICE_HOST_PORT: 'localhost:2020',
+
+    // Web application context path e.g. / (for http://127.0.0.1/) or /reconmap (for http://127.0.0.1/reconmap)
+    // REACT_APP_CONTEXT_PATH: '/reconmap'
 };
