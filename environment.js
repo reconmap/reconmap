aba0dec4f3d871db1bf390950ec5fde391903124
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2023-08-11 22:18:18 +0100

    Update default environment file

--- environment.js
+++ environment.js
@@ -1,19 +1,21 @@
 window.env = {
-    REACT_APP_SECURE_TRANSPORT_ENABLED: false,
+    VITE_SECURE_TRANSPORT_ENABLED: false,
 
     // Reconmap API's URL including protocol and port but not trailing slash
-    REACT_APP_DEFAULT_API_URL: 'http://localhost:5510',
+    VITE_DEFAULT_API_URL: 'http://localhost:5510',
 
     // hostname:port for the Reconmap's notifications service
-    REACT_APP_NOTIFICATIONS_SERVICE_HOST_PORT: 'localhost:5520',
+    VITE_NOTIFICATIONS_SERVICE_HOST_PORT: 'localhost:5520',
 
     // hostname:port for the Reconmap's agent service
-    REACT_APP_AGENT_SERVICE_HOST_PORT: 'localhost:5520',
+    VITE_AGENT_SERVICE_HOST_PORT: 'localhost:5520',
 
-    // Web application context path e.g. / (for http://127.0.0.1:5500) or /reconmap (for http://127.0.0.1:5500/reconmap)
-    // REACT_APP_CONTEXT_PATH: '/reconmap',
+    VITE_LOGO_URL: 'logo-name.png',
 
-    REACT_APP_KEYCLOAK_CONFIG: {
+    // Web application context path e.g. / (for http://localhost:5500) or /reconmap (for http://localhost:5500/reconmap)
+    // VITE_CONTEXT_PATH: '/reconmap'
+
+    VITE_KEYCLOAK_CONFIG: {
         url: 'http://localhost:8080',
         realm: 'reconmap',
         clientId: 'web-client'
