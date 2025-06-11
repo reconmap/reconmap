39e653778fb47934aac2888bd5030a5d46f6fe33
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2025-06-12 00:01:16 +0100

    Update config ui file

--- config-ui.js
+++ config-ui.js
@@ -1,23 +1,12 @@
-window.env = {
-    VITE_SECURE_TRANSPORT_ENABLED: false,
-
-    // Reconmap API's URL including protocol and port but not trailing slash
-    VITE_DEFAULT_API_URL: 'http://localhost:5510',
-
-    // hostname:port for the Reconmap's notifications service
-    VITE_NOTIFICATIONS_SERVICE_HOST_PORT: 'localhost:5520',
-
-    // hostname:port for the Reconmap's agent service
-    VITE_AGENT_SERVICE_HOST_PORT: 'localhost:5520',
-
-    VITE_LOGO_URL: '/logo-name.png',
-
-    // Web application context path e.g. / (for http://localhost:5500) or /reconmap (for http://localhost:5500/reconmap)
-    // VITE_CONTEXT_PATH: '/reconmap'
-
-    VITE_KEYCLOAK_CONFIG: {
-        url: 'http://localhost:8080',
-        realm: 'reconmap',
-        clientId: 'web-client'
+{
+    "contextPath": "",
+    "logoUrl": "/logo-name.png",
+    "reconmapApiUrl": "http://localhost:5510",
+    "notificationsServiceUrl": "ws://localhost:5520",
+    "agentServiceUrl": "ws://localhost:5520",
+    "keycloak": {
+        "url": "http://localhost:8080",
+        "realm": "reconmap",
+        "clientId": "web-client"
     }
-};
+}
