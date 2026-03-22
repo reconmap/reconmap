06ee3dc9d1961dad5e79644886f35083dcc0b6c9
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2026-03-22 23:00:23 +0000

    Fix test

--- apps/dashboard/src/layouts/SidemenuLayout.test.jsx
+++ apps/dashboard/src/layouts/SidemenuLayout.test.jsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { MemoryRouter } from "react-router-dom";
-import SidemenuLayout from "../components/layout/SidemenuLayout.jsx";
+import SidemenuLayout from "./SidemenuLayout.jsx";
 
 describe("SidemenuLayout", () => {
     const links = [
